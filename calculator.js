cd0fa7e4f89359a523ba97eba71092bcd4342a8d
Henna Fatemah <henna_fatemah@yahoo.com>
2021-03-18 17:11:15 -0400

    final commit

--- calculator.js
+++ calculator.js
@@ -1,108 +1,126 @@
 class Calculator {
-    constructor(displayTextElement){
-        this.displayTextElement = displayTextElement;
-        this.clearAll()
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
     }
-
-    clearAll(){
-        this.display = ''
-        this.operator = undefined
+  
+    clear() {
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined
     }
-    return(){
-
+  
+    delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
-    appendNumber(number){
-        this.display = number;
+  
+    appendNumber(number) {
+      if (number === '.' && this.currentOperand.includes('.')) return
+      this.currentOperand = this.currentOperand.toString() + number.toString()
     }
-    pressOperator(operator){
-
+  
+    chooseOperation(operation) {
+      if (this.currentOperand === '') return
+      if (this.previousOperand !== '') {
+        this.compute()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
     }
-    compute(){
-
+  
+    compute() {
+      let computation
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if (isNaN(prev) || isNaN(current)) return
+      switch (this.operation) {
+        case '+':
+          computation = prev + current
+          break
+        case '-':
+          computation = prev - current
+          break
+        case '*':
+          computation = prev * current
+          break
+        case '÷':
+          computation = prev / current
+          break
+        default:
+          return
+      }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
     }
-    updateDisplay(){
-        this.displayTextElement.innerText = this.display;
+  
+    getDisplayNumber(number) {
+      const stringNumber = number.toString()
+      const integerDigits = parseFloat(stringNumber.split('.')[0])
+      const decimalDigits = stringNumber.split('.')[1]
+      let integerDisplay
+      if (isNaN(integerDigits)) {
+        integerDisplay = ''
+      } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+      }
+      if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`
+      } else {
+        return integerDisplay
+      }
     }
-}
-
-
-
-
-const allClear = document.querySelector('all-clear');
-const equalsTo = document.querySelector('equals');
-const backSpace = document.querySelector('back-space');
-const operators = document.querySelectorAll('operator');
-const numbers = document.querySelectorAll('number');
-const display = document.querySelector('display');
-
-const calculator = new Calculator(displayTextElement);
-
-numbers.forEach(calcButton => {
-    calcButton.addEventListener('click', () => {
-        calculator.appendNumber(calcButton.innerText)
-        calculator.updateDisplay()
+  
+    updateDisplay() {
+      this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+      if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      } else {
+        this.previousOperandTextElement.innerText = ''
+      }
+    }
+  }
+  
+  
+  const numberButtons = document.querySelectorAll('[data-number]')
+  const operationButtons = document.querySelectorAll('[data-operation]')
+  const equalsButton = document.querySelector('[data-equals]')
+  const deleteButton = document.querySelector('[data-delete]')
+  const allClearButton = document.querySelector('[data-all-clear]')
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
     })
-})
-
-// function zero() {
-//     return 0;
-// }
-// function one() {
-//     return 1;
-// }
-
-// let digitOne, digitTwo, digitThree, digitFour, digitFive, digitSix, digitSeven, digitEight;
-
-
-// function firstNumber() {
-//     if (digitOne === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) {
-//         record(digitOne);
-//     } else if (digitTwo === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record(10 * digitOne + digitTwo);
-//     } else if (digitThree === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((100 * digitOne) + (10 * digitTwo) + digitThree);
-//     } else if (digitFour === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((1000 * digitOne) + (100 * digitTwo) + (10 * digitThree) + (digitFour));
-//     } else if (digitFive === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((10000 * digitOne) + (1000 * digitTwo) + (100 * digitThree) + (10 * digitFour) + (digitFive));
-//     } else if (digitSix === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((100000 * digitOne) + (10000 * digitTwo) + (1000 * digitThree) + (100 * digitFour) + (10 * digitFive) + (digitSix));
-//     } else if (digitSeven === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((1000000 * digitOne) + (100000 * digitTwo) + (10000 * digitThree) + (1000 * digitFour) + (100 * digitFive) + (10 * digitSix) + digitSeven);
-//     } else if (digitEight === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((10000000 * digitOne) + (1000000 * digitTwo) + (100000 * digitThree) + (10000 * digitFour) + (1000 * digitFive) + (100 * digitSix) + (10 * digitSeven) + digitEight);
-//     } else if (digitNine === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((100000000 * digitOne) + (10000000 * digitTwo) + (1000000 * digitThree) + (100000 * digitFour) + (10000 * digitFive) + (1000 * digitSix) + (100 * digitSeven) + (10 * digitEight) + digitNine);
-//     } else {
-//         record ("error");
-//     }
-
-//     return record;
-// }
-
-// function secondNumber() {
-//     if (digitOne === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) {
-//         record(digitOne);
-//     } else if (digitTwo === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record(10 * digitOne + digitTwo);
-//     } else if (digitThree === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((100 * digitOne) + (10 * digitTwo) + digitThree);
-//     } else if (digitFour === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((1000 * digitOne) + (100 * digitTwo) + (10 * digitThree) + (digitFour));
-//     } else if (digitFive === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((10000 * digitOne) + (1000 * digitTwo) + (100 * digitThree) + (10 * digitFour) + (digitFive));
-//     } else if (digitSix === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((100000 * digitOne) + (10000 * digitTwo) + (1000 * digitThree) + (100 * digitFour) + (10 * digitFive) + (digitSix));
-//     } else if (digitSeven === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((1000000 * digitOne) + (100000 * digitTwo) + (10000 * digitThree) + (1000 * digitFour) + (100 * digitFive) + (10 * digitSix) + digitSeven);
-//     } else if (digitEight === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((10000000 * digitOne) + (1000000 * digitTwo) + (100000 * digitThree) + (10000 * digitFour) + (1000 * digitFive) + (100 * digitSix) + (10 * digitSeven) + digitEight);
-//     } else if (digitNine === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
-//         record((100000000 * digitOne) + (10000000 * digitTwo) + (1000000 * digitThree) + (100000 * digitFour) + (10000 * digitFive) + (1000 * digitSix) + (100 * digitSeven) + (10 * digitEight) + digitNine);
-//     } else {
-//         record ("error");
-//     }
-
-//     return record;
-// }
-//     document.getElementById("demo").innerHTML = zero() || one();
+  })
+  
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
+  equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+  })
+  
+  allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+  })
+  
+  deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+  })
